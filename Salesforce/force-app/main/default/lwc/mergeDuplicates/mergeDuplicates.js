81f9bee4c631b88ff7246fb45e5b1c735516b810
Gaurav Arora <gaurav.arora@grabtaxi.com>
2024-06-24 00:41:50 +0530

    Merge Duplicate Project

---
+++ Salesforce/force-app/main/default/lwc/mergeDuplicates/mergeDuplicates.js
@@ -0,0 +1,84 @@
+/**
+ * @description       : This js is used to display the duplicate records for a given record type
+ * @author            : Kapil Arora
+ * @group             : 
+ * @last modified on  : 22-06-2024
+ * @last modified by  : Kapil Arora
+ * Modifications Log 
+ * Ver   Date         Author                               Modification
+ * 1.0   22-06-2024   Kapil Arora                          Initial Version
+**/
+import { LightningElement, api, wire } from 'lwc';
+import getDuplicateRecords from '@salesforce/apex/DuplicateRecordsHandler.getDuplicateRecords';
+import { CloseActionScreenEvent } from 'lightning/actions';
+import { refreshApex } from '@salesforce/apex';
+const actions = [
+    { label: 'Merge This', name: 'merge_record' }
+];
+export default class MergeDuplicates extends LightningElement {
+    @api
+    recordId;
+    wiredDataResult;
+    columns
+    showDupRecords
+    mergeRecords
+    ifRecordSelected
+    wiredValues
+
+    @wire(getDuplicateRecords, {
+        record: '$recordId',
+        offset: 0,
+        limitSize: 150
+        
+    })
+    wiredData(response) {
+        this.wiredValues = response
+        const {data,error} = response
+        if(data){
+            this.wiredDataResult = []
+            this.columns = [
+                {  
+                    label: "Record Link",  
+                    fieldName: "recordLink",  
+                    type: "url",  
+                    typeAttributes: { label: { fieldName: "Name" }, tooltip:"Name", target: "_blank" }  
+                } 
+            ];
+            this.wiredDataResult = data.data;
+            this.wiredDataResult = this.wiredDataResult.map(item=>{
+                item = {...item, ['recordLink']:'/'+item.Id}
+                return item
+            })
+            let columnsData = Array.from(data.columns)
+            this.columns.splice(this.columns.length, 0, ...columnsData);
+            this.columns.push({ type: 'action', typeAttributes: { rowActions: actions, menuAlignment: 'auto' } })
+            this.showDupRecords = true
+        }
+        if(error){
+            this.template.querySelector("c-merge-duplicates-child").showNotification('Error',error.body.message,'error')
+            this.handleClose()
+        }
+    }
+
+    handleRowAction(event) {
+        const action = event.detail.action;
+        const row = event.detail.row;
+        this.ifRecordSelected = true
+        this.showDupRecords = false
+        this.mergeRecords = row.Id+'--'+ this.recordId;
+        this.template.querySelector("c-merge-duplicates-child").refreshData()
+    }
+
+    handlePrev(){
+        this.ifRecordSelected = false
+        this.showDupRecords = true
+    }
+    handleClose(){
+        this.dispatchEvent(new CloseActionScreenEvent());
+    }
+
+    refreshHandler(){
+        refreshApex(this.wiredValues)
+    }
+    
+}
---
+++ Salesforce/force-app/main/default/lwc/mergeDuplicatesChild/mergeDuplicatesChild.js
@@ -0,0 +1,117 @@
+/**
+ * @description       : This js is used to display records for parent and selected duplicate record 
+ *                      as well as to merge the record
+ * @author            : Kapil Arora
+ * @group             : 
+ * @last modified on  : 22-06-2024
+ * @last modified by  : Kapil Arora
+ * Modifications Log 
+ * Ver   Date         Author                               Modification
+ * 1.0   22-06-2024   Kapil Arora                          Initial Version
+**/
+import { LightningElement, api, wire } from 'lwc';
+import { ShowToastEvent} from 'lightning/platformShowToastEvent';
+import getMergeRecordsData from '@salesforce/apex/DuplicateRecordsHandler.getMergeRecordsData';
+import { updateRecord } from "lightning/uiRecordApi";
+import doReparenting from '@salesforce/apex/DuplicateRecordsHandler.doReparenting';
+import { refreshApex } from '@salesforce/apex';
+
+export default class MergeDuplicatesChild extends LightningElement {
+
+    @api
+    recordIds;
+
+    value
+    mergedRecords
+    wiredValues
+    fieldsDataToMerge = {}
+    @api
+    ifRecordSelected = false
+    @wire(getMergeRecordsData, {
+        records: '$recordIds'
+    })wiredData(response) {
+        this.wiredValues = response
+        const {data,error} = response
+        if(data){
+            this.mergedRecords = []
+            this.fieldsDataToMerge = {}
+            this.mergedRecords = Object.values(data).filter(result=>{
+                return result.val1 && result.val2 && result.val1 !== result.val2
+            })
+        }
+        if(error){
+            this.showNotification('Error',error.body.message,'error')
+        }
+    }
+
+
+    handleMerge(){
+
+        if(JSON.stringify(this.fieldsDataToMerge) === "{}"){
+            this.showNotification('Error','Please select fields to merge','error')
+        }else{
+            console.log(JSON.stringify(this.fieldsDataToMerge,null,2))
+            let fields = {...this.fieldsDataToMerge, ['Id']:this.recordIds.split('--')[1]}
+            console.log(JSON.stringify(fields,null,2))
+            const recordInput = { fields }
+            updateRecord(recordInput).then(result=>{
+                refreshApex(this.wiredValues)
+                doReparenting({records : this.recordIds})
+                this.dispatchEvent(new CustomEvent("refresh"));
+                this.showNotification('Success','Successfully Merged')
+                this.dispatchEvent(new CustomEvent("close"))
+            }).catch(error=>{
+                this.showNotification('Error',error.body.message,'error')
+            })
+        }
+        
+    }
+
+    handleCancel(){
+        this.dispatchEvent(new CustomEvent("refresh"));
+        this.dispatchEvent(new CustomEvent("close"))
+    }
+
+    handlePrev(){
+        this.dispatchEvent(new CustomEvent("refresh"));
+        this.dispatchEvent(new CustomEvent("previous"));
+    }
+
+    @api
+    showNotification(title,message,variant){
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title:title,
+                message:message,
+                variant: variant || 'success' 
+            })
+        )
+    }
+
+    handleChange(event){
+        const {name,value} = event.target
+        this.fieldsDataToMerge = {...this.fieldsDataToMerge, [name]:value}
+        console.log(JSON.stringify(this.fieldsDataToMerge,null,2))
+    }
+
+    handleSelectAll(event){
+        const {name,value} = event.target
+        if(value=='Master'){
+            this.mergedRecords.map(result=>{
+                const fieldname = result.fieldName
+                this.template.querySelectorAll(`input[name="${fieldname}"]`)[0].click()
+            })
+        }else{
+            this.mergedRecords.map(result=>{
+                const fieldname = result.fieldName
+                this.template.querySelectorAll(`input[name="${fieldname}"]`)[1].click()
+            })
+        }
+    }
+
+    @api
+    refreshData(){
+        refreshApex(this.wiredValues)
+    }
+    
+}
